1c85064af53ee2362bed5bb7134ec46be8d55222
J0nathanbell <jonnybell123@hotmail.com>
2022-12-06 13:11:11 +0000

    altered monday

--- monday.js
+++ monday.js
@@ -18,6 +18,7 @@ async function inputData(data) {
 
   await page.locator('xpath=//*[@id="board-header-view-bar"]/div[1]/div[2]/div/div[1]/button').click()
 
+  //*[@id="row-pulse---3597279347-notplaceholder-focus-name-"]/div/div[4]/div
   // const browser = await chromium.launch({
   //   headless: false
   // });
